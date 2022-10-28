522d018e5e7e07bca5f58cd9bd468e39f66b76dc
mynameisvip <46710942+mynameisvip@users.noreply.github.com>
2022-10-28 14:43:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
 import requests
 import openpyxl
 
-file = "excel.xlsx"
-from_row = 0
-to_row = 0
-origin_column = "A"
-target_column = "B"
-API_key = "YOUR_GOOGLE_MAPS_API_KEY"
+FILE = "excel.xlsx"
+FROM_ROW = 0
+TO_ROW = 0
+ORIGIN_COLUMN = "A"
+TARGET_COLUMN = "B"
+API_KEY = "YOUR_GOOGLE_MAPS_API_KEY"
 
 book = openpyxl.load_workbook(file)
 sheet = book.active
 
 
-for i in range(from_row, to_row):
+for i in range(FROM_ROW, TO_ROW):
     try:
-        origin = sheet[f'{origin_column}{i}'].value
-        destination = sheet[f'{target_column}{i}'].value
+        origin = sheet[f'{ORIGIN_COLUMN}{i}'].value
+        destination = sheet[f'{TARGET_COLUMN}{i}'].value
         if not origin or not destination:
             break
-        url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={origin}&destinations={destination}&units=metric&key={API_key}"
-        payload = {}
-        headers = {}
-        response = requests.request("GET", url, headers=headers, data=payload)
+        url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={origin}&destinations={destination}&units=metric&key={API_KEY}"
+        response = requests.request("GET", url, headers={}, data={})
         sheet[f'C{i}'] = response.json()["rows"][0]["elements"][0]["distance"]["text"]
     except:
         pass
